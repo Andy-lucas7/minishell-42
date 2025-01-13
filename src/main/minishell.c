39ca1cd186f4d543ad6a3b37d594f919d08af274
Lucas Andrey Serrao Muniz <lserrao-@student.42sp.org.br>
2025-01-13 19:45:46 -0300

    Updated: src/main/minishell.c

--- src/main/minishell.c
+++ src/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lserrao- <lserrao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 20:46:53 by lserrao-          #+#    #+#             */
-/*   Updated: 2025/01/09 21:24:52 by lserrao-         ###   ########.fr       */
+/*   Updated: 2025/01/13 19:35:00 by lserrao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,44 +28,99 @@ static char	*join_path(const char *dir, const char *cmd)
 	return (path);
 }
 
-static char	*find_command(const char *cmd, char **envp)
+// Libera memória de uma lista de strings
+static void	free_paths(char **paths)
 {
-	char	*path_env = NULL;
-	char	**paths;
-	char	*full_path;
-	int		i;
+	int	i;
+
+	if (!paths)
+		return ;
+	i = 0;
+	while (paths[i])
+	{
+		free(paths[i]);
+		i++;
+	}
+	free(paths);
+}
+
+// Retorna a variável PATH do ambiente
+static char	*get_path_env(char **envp)
+{
+	int	i;
 
 	i = 0;
 	while (envp[i])
 	{
 		if (strncmp(envp[i], "PATH=", 5) == 0)
-		{
-			path_env = envp[i] + 5;
-			break ;
-		}
+			return (envp[i] + 5);
 		i++;
 	}
-	if (!path_env)
+	return (NULL);
+}
+
+// Cria uma lista de paths a partir do PATH
+static char	**get_paths(const char *path_env)
+{
+	char	**paths;
+	char	*token;
+	char	*copy;
+	int		i;
+
+	copy = strdup(path_env);
+	if (!copy)
 		return (NULL);
 	paths = malloc(sizeof(char *) * 256);
 	if (!paths)
+	{
+		free(copy);
+		return (NULL);
+	}
+	i = 0;
+	token = strtok(copy, ":");
+	while (token && i < 255)
+	{
+		paths[i] = strdup(token); // Duplica cada token para evitar problemas de memória
+		if (!paths[i++])
+		{
+			free_paths(paths);
+			free(copy);
+			return (NULL);
+		}
+		token = strtok(NULL, ":");
+	}
+	paths[i] = NULL;
+	free(copy); // Libera a cópia original
+	return (paths);
+}
+
+// Procura o comando nos diretórios do PATH
+static char	*find_command(const char *cmd, char **envp)
+{
+	char	**paths;
+	char	*full_path;
+	char	*path_env;
+	int		i;
+
+	path_env = get_path_env(envp);
+	if (!path_env)
+		return (NULL);
+	paths = get_paths(path_env);
+	if (!paths)
 		return (NULL);
 	i = 0;
-	paths[i] = strtok(path_env, ":");
-	while (paths[i] && i < 255)
-		paths[++i] = strtok(NULL, ":");
-	i = -1;
-	while (paths[++i])
+	while (paths[i])
 	{
 		full_path = join_path(paths[i], cmd);
 		if (full_path && access(full_path, X_OK) == 0)
 		{
-			free(paths);
+			free_paths(paths);
 			return (full_path);
 		}
 		free(full_path);
+		i++;
 	}
-	free(paths);
+	free_paths(paths);
 	return (NULL);
 }
 
@@ -78,7 +133,7 @@ static void	execute_command(char **args, char **envp)
 	cmd_path = find_command(args[0], envp);
 	if (!cmd_path)
 	{
-		fprintf(stderr, "minishell: %s: command not found\n", args[0]);
+		printf("\033[1;36mMinishell:\033[0m %s: command not found\n", args[0]);
 		return ;
 	}
 	pid = fork();
@@ -91,12 +146,15 @@ static void	execute_command(char **args, char **envp)
 		}
 	}
 	else if (pid > 0)
+	{
 		waitpid(pid, &status, 0);
+	}
 	else
 		perror("fork");
 	free(cmd_path);
 }
 
+// Divide a entrada em tokens
 static char	**tokenize_input(char *input)
 {
 	char	**args;
@@ -111,21 +169,25 @@ static char	**tokenize_input(char *input)
 	i = 0;
 	args[i] = strtok(input, " ");
 	while (args[i] && i < 255)
+	{
 		args[++i] = strtok(NULL, " ");
+	}
 	args[i] = NULL;
 	return (args);
 }
 
+// Captura a entrada do usuário
 static char	*get_prompt(void)
 {
 	char	*input;
 
-	input = readline("\033[0;36mMinishell: \033[0m");
+	input = readline("\033[1;36mMinishell: \033[0m");
 	if (input && *input)
 		add_history(input);
 	return (input);
 }
 
+// Função principal
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
@@ -145,7 +207,10 @@ int	main(int argc, char **argv, char **envp)
 		{
 			args = tokenize_input(input);
 			if (args && strcmp(args[0], "exit") == 0)
+			{
+				free(args);
 				break ;
+			}
 			if (args)
 				execute_command(args, envp);
 			free(args);
