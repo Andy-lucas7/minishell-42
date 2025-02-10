2333a8a0331e38820ffeb0399dee36d632f3165b
Lucas Andrey Serrao Muniz <lserrao-@student.42sp.org.br>
2025-02-10 17:41:39 -0300

    update: bi_exit.c

--- src/builtins/bi_env.c
+++ src/builtins/bi_env.c
@@ -6,7 +6,7 @@
 /*   By: lserrao- <lserrao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:16:40 by jreis-do          #+#    #+#             */
-/*   Updated: 2025/02/05 19:26:38 by lserrao-         ###   ########.fr       */
+/*   Updated: 2025/02/10 17:34:20 by lserrao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	bi_env(t_mini *ms, char **args, char ***envp)
 		ft_putstr_fd("-minishell: env: invalid usage\n", 2);
 		ms->error = 42;
 	}
+	if (!strcmp(args[0], ))
 	else
 	{
 		count = 0;
