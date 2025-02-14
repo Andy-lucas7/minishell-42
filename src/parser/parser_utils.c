40105936a4eb1bb2be51b1726ba2777e8fff2a4f
Lucas Andrey Serrao Muniz <lserrao-@student.42sp.org.br>
2025-02-14 20:48:55 -0300

    Updated: parser_utils.c

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: lserrao- <lserrao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 18:18:42 by lserrao-          #+#    #+#             */
-/*   Updated: 2025/02/03 18:23:21 by lserrao-         ###   ########.fr       */
+/*   Updated: 2025/02/14 20:47:00 by lserrao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	check_pipe(char *cmd, t_token **head)
 {
-	char	*input;
+	char				*input;
 
 	input = NULL;
-	if (!ft_strncmp(cmd, "|", 2))
+	if (!ft_strncmp(cmd, "|", 2) || cmd[1] == '\0')
 	{
 		input = readline("> ");
 		if (input && !input[0])
